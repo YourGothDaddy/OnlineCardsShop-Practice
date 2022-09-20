bb0d13d58a0cf40c86c8d7ee715b21bbc4666ea1
GothDaddy <alexanderorjustalex@gmail.com>
2022-09-20 14:04:02 +0300

    Added 'All Kpop Cards' view that can be searched, sorted and browsed through

--- OnlineCardShop/Controllers/HomeController.cs
+++ OnlineCardShop/Controllers/HomeController.cs
@@ -3,6 +3,7 @@
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Logging;
     using OnlineCardShop.Data;
+    using OnlineCardShop.Data.Models.Enums;
     using OnlineCardShop.Models;
     using OnlineCardShop.Models.Cards;
     using OnlineCardShop.Models.Home;
@@ -34,6 +35,55 @@
             {
                 Categories = categories
             });
+        }
+
+        public IActionResult Kpop([FromQuery] AllCardsQueryModel query, [FromQuery] int currentPage)
+        {
+            var cardsQuery = this.data.Cards
+                .Where(c => c.CategoryId == 1)
+                .AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(query.SearchTerm))
+            {
+                cardsQuery = cardsQuery
+                    .Where(c =>
+                    c.Title.ToLower().Contains(query.SearchTerm.ToLower()));
+            }
+
+            cardsQuery = query.Sorting switch
+            {
+                CardSorting.Condition => cardsQuery.OrderBy(c => c.Condition),
+                CardSorting.Category => cardsQuery.OrderByDescending(c => c.Category),
+                _ => cardsQuery.OrderByDescending(c => c.Condition)
+            };
+
+            var totalCards = cardsQuery.Count();
+
+            var cards = cardsQuery
+                .Skip((query.CurrentPage - 1) * AllCardsQueryModel.CardsPerPage)
+                .Take(AllCardsQueryModel.CardsPerPage)
+                .Select(c => new CardListingViewModel
+                {
+                    Title = c.Title,
+                    Description = c.Description,
+                    ImageUrl = c.ImageUrl,
+                    Category = c.Category.Name,
+                    Condition = c.Condition.Name
+
+                })
+                .ToList();
+
+            query.TotalCards = totalCards;
+            query.Cards = cards;
+            if (currentPage == 0)
+            {
+                ViewBag.CurrentPage = 1;
+            }
+            else
+            {
+                ViewBag.CurrentPage = currentPage;
+            }
+            return View(query); ;
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]

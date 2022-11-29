5d5dbf0a9db6f587baa24ae367683006e1e82aa9
GothDaddy <alexanderorjustalex@gmail.com>
2022-11-29 08:21:17 +0200

    Introduced admin access to user profile and their reports

---
+++ OnlineCardShop/Areas/Admin/Controllers/DealersController.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineCardShop.Areas.Admin.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class DealersController : AdminController
+    {
+        public IActionResult Dealer()
+        {
+            return View();
+        }
+    }
+}
